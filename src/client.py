d540a73bd89c01aab8343902b3151c5fc7bc4991
Draekus <patrick.gould94@gmail.com>
2023-12-30 22:04:45 -0500

    Added basic file sharing server with ECDF key exchange.

--- src/client.py
+++ src/client.py
@@ -1,29 +1,101 @@
-import socket
+"""Dummy client module for testing the server."""
 import os
+import socket
+from dotenv import load_dotenv
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization, hashes
+
+# Load environment variables
+if os.path.exists(".env"):
+    load_dotenv(".env")
+
+# Server configuration
+IP = os.getenv("SERVER_ADDRESS")
+PORT = int(os.getenv("PORT"))
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
 
 
-def send_file(server_address, port, file_path):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_address, port))
+def generate_ecdh_key_pair():
+    """Generates a new ECDH key pair."""
+    private_key = ec.generate_private_key(ec.SECP256R1(), default_backend())
+    public_key = private_key.public_key()
+    return private_key, public_key
 
-    file_name = os.path.basename(file_path)
-    file_size = os.path.getsize(file_path)
 
-    # client_socket.send(file_name.encode())
-    client_socket.send(str(file_size).encode())
+def perform_ecdh_key_exchange(conn):
+    """Performs ECDH key exchange with the server."""
+    # Generate ECDH key pair
+    client_private_key, client_public_key = generate_ecdh_key_pair()
 
-    print(f"Sending {file_name} ({file_size} bytes)")
+    # Send client's public key to the server
+    serialized_public_key = client_public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
+    )
+    conn.send(serialized_public_key)
 
-    with open(file_path, "rb") as file:
-        for data in iter(lambda: file.read(1024), b""):
-            client_socket.sendall(data)
+    # Receive server's public key
+    server_public_key_bytes = conn.recv(SIZE)
+    server_public_key = serialization.load_pem_public_key(
+        server_public_key_bytes, backend=default_backend()
+    )
 
-    print("File sent successfully")
-    client_socket.close()
+    # Perform ECDH key exchange
+    shared_key = client_private_key.exchange(ec.ECDH(), server_public_key)
+
+    # You can use shared_key as a symmetric key for encryption, e.g., using a KDF.
+
+    return shared_key
+
+
+def main():
+    """Main function."""
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+    shared_key = perform_ecdh_key_exchange(client)
+    print(f"[SERVER]: {shared_key}")
+
+    while True:
+        data = client.recv(SIZE).decode(FORMAT)
+        cmd, msg = data.split("@")
+
+        if cmd == "DISCONNECTED":
+            print(f"[SERVER]: {msg}")
+            break
+        elif cmd == "OK":
+            print(f"{msg}")
+
+        data = input("> ")
+        data = data.split(" ")
+        cmd = data[0]
+
+        if cmd == "HELP":
+            client.send(cmd.encode(FORMAT))
+        elif cmd == "LOGOUT":
+            client.send(cmd.encode(FORMAT))
+            break
+        elif cmd == "LIST":
+            client.send(cmd.encode(FORMAT))
+        elif cmd == "DELETE":
+            client.send(f"{cmd}@{data[1]}".encode(FORMAT))
+        elif cmd == "UPLOAD":
+            filepath = data[1]
+
+            filename = os.path.basename(filepath)
+            filesize = os.path.getsize(filepath)
+
+            client.send(f"UPLOAD@{filename}@{filesize}".encode(FORMAT))
+
+            with open(filepath, "rb") as f:
+                for chunk in iter(lambda: f.read(SIZE), b""):
+                    client.send(chunk)
+
+    print("Disconnected from the server.")
+    client.close()
 
 
 if __name__ == "__main__":
-    server_address = "127.0.0.1"  # Change to the server's IP address
-    port = 61111  # Change to the server's port number
-    file_path = "../res/data.txt"  # Change to the path of the file you want to send
-    send_file(server_address, port, file_path)
+    main()
--- src/server.py
+++ src/server.py
@@ -1,40 +1,154 @@
-import socket
+"""Multi-threaded server that transfers files between clients."""
 import os
+import socket
+import threading
+from dotenv import load_dotenv
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization, hashes
+from typing import Dict, Any
+
+# Load environment variables
+if os.path.exists(".env"):
+    load_dotenv(".env")
+
+# Server configuration
+IP = os.getenv("SERVER_ADDRESS")
+PORT = int(os.getenv("PORT"))
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+SERVER_DATA_PATH = os.getenv("SERVER_DATA_PATH")
+
+# Map socket connections to shared keys
+shared_keys: Dict[Any, bytes] = {}
 
 
-def receive_file(server_address, port):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((server_address, port))
-    server_socket.listen(5)
+# ECDH key pair generation
+def generate_ecdh_key_pair() -> tuple:
+    """Generates a new ECDH key pair."""
+    private_key = ec.generate_private_key(ec.SECP256R1(), default_backend())
+    public_key = private_key.public_key()
+    return private_key, public_key
 
-    print(f"Server listening on {server_address}:{port}")
 
-    client_socket, client_address = server_socket.accept()
-    print(f"Connection established with {client_address}")
+def perform_ecdh_key_exchange(conn) -> bytes:
+    """Performs ECDH key exchange with the client."""
+    # Generate ECDH key pair
+    server_private_key, server_public_key = generate_ecdh_key_pair()
 
-    # Receive file name
-    # file_name = client_socket.recv(1024).decode()
-    # print(f"Receiving {file_name}")
+    # Send server's public key to the client
+    serialized_public_key = server_public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
+    )
+    conn.send(serialized_public_key)
 
-    # Receive file size as a string
-    file_size_str = client_socket.recv(1024)
+    # Receive client's public key
+    client_public_key_bytes = conn.recv(SIZE)
+    client_public_key = serialization.load_pem_public_key(
+        client_public_key_bytes, backend=default_backend()
+    )
 
-    file_size = int(file_size_str.decode())
+    # Perform ECDH key exchange
+    shared_key = server_private_key.exchange(ec.ECDH(), client_public_key)
 
-    print(f"Receiving ({file_size} bytes)")
+    return shared_key
 
-    with open("received.txt", "wb") as file:
-        while file_size > 0:
-            data = client_socket.recv(1024)
-            file.write(data)
-            file_size -= len(data)
 
-    print("File received successfully")
-    client_socket.close()
-    server_socket.close()
+def handle_client(conn, addr) -> None:
+    """Handles a new client connection."""
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    # Perform ECDH key exchange
+    shared_key = perform_ecdh_key_exchange(conn)
+
+    # Store the shared key
+    shared_keys[conn] = shared_key
+    print(f"[SHARED KEY] Address: {addr} Key: {shared_key}")
+
+    conn.send("OK@Welcome to the File Server.".encode(FORMAT))
+
+    while True:
+        data = conn.recv(SIZE).decode(FORMAT)
+        data = data.split("@")
+        cmd = data[0]
+
+        if cmd == "LIST":
+            files = os.listdir(SERVER_DATA_PATH)
+            send_data = "OK@"
+
+            if len(files) == 0:
+                send_data += "The server directory is empty"
+            else:
+                send_data += "\n".join(f for f in files)
+            conn.send(send_data.encode(FORMAT))
+
+        elif cmd == "UPLOAD":
+            filename, filesize = data[1], int(data[2])
+            filepath = os.path.join(SERVER_DATA_PATH, filename)
+
+            with open(filepath, "wb") as f:
+                received_data = 0
+                while received_data < filesize:
+                    chunk = conn.recv(SIZE)
+                    if not chunk:
+                        break
+                    f.write(chunk)
+                    received_data += len(chunk)
+
+            send_data = "OK@File uploaded successfully."
+            conn.send(send_data.encode(FORMAT))
+
+        elif cmd == "DELETE":
+            files = os.listdir(SERVER_DATA_PATH)
+            send_data = "OK@"
+            filename = data[1]
+
+            if len(files) == 0:
+                send_data += "The server directory is empty"
+            else:
+                if filename in files:
+                    os.system(f"rm {SERVER_DATA_PATH}/{filename}")
+                    send_data += "File deleted successfully."
+                else:
+                    send_data += "File not found."
+
+            conn.send(send_data.encode(FORMAT))
+
+        elif cmd == "LOGOUT":
+            break
+        elif cmd == "HELP":
+            data = "OK@"
+            data += "LIST: List all the files from the server.\n"
+            data += "UPLOAD <path>: Upload a file to the server.\n"
+            data += "DELETE <filename>: Delete a file from the server.\n"
+            data += "LOGOUT: Disconnect from the server.\n"
+            data += "HELP: List all the commands."
+
+            conn.send(data.encode(FORMAT))
+
+        else:
+            conn.send("OK@Invalid command.".encode(FORMAT))
+
+    print(f"[DISCONNECTED] {addr} disconnected")
+    conn.close()
+
+
+def main():
+    """Main function."""
+    print("[STARTING] Server is starting")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    server.listen()
+    print(f"[LISTENING] Server is listening on {IP}:{PORT}.")
+
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
 
 if __name__ == "__main__":
-    server_address = "127.0.0.1"  # Change to your desired IP address
-    port = 61111  # Change to your desired port number
-    receive_file(server_address, port)
+    main()
